a73b509e707b608edc9cf8dddec9a749758297e0
Sharon Levi <73107447+sharonlevi94@users.noreply.github.com>
2020-12-25 12:20:09 +0200

    Update ex6a2.c

--- ex6a2.c
+++ ex6a2.c
@@ -1,4 +1,3 @@
-//client program: 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,12 +10,17 @@
 #define  YES 1
 #define  NO  0
 #define  WANT_TO_CONNECT 2
-#define  FINISH -1
+const int FINISH= -1;
 //========================================================================
 void terminate(char *);
 //========================================================================
 int main(int argc, char *argv[])
 {
+	 if (argc < 4) {
+	        fprintf(stderr, "Missing server name \n");
+	        exit(EXIT_FAILURE) ;
+	    }
+
 	srand               (atoi(argv[1]));
     int                 rc;            /* system calls return value storage */
     int                 my_socket;
@@ -26,16 +30,11 @@ int main(int argc, char *argv[])
                         *addr_info_res ;
     int                 nums_sent=0,nums_deleted=0;
 
-    if (argc < 4) {
-        fprintf(stderr, "Missing server name \n");
-        exit(EXIT_FAILURE) ;
-    }
-
     memset(&con_kind, 0, sizeof(con_kind)) ;
     con_kind.ai_family = AF_UNSPEC ;        // AF_INET, AF_INET6
     con_kind.ai_socktype = SOCK_STREAM ;
 
-    if ((rc = getaddrinfo(argv[1], argv[3],//<-server port number -argv[3]
+    if ((rc = getaddrinfo(argv[2], argv[3],//<-server port number -argv[3]
                                      &con_kind,
                                      &addr_info_res) != 0)) {
        fprintf(stderr, "(getaddrinfo() failed %s\n", gai_strerror(rc)) ;
@@ -58,25 +57,28 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE) ;
     }
 
-    //missed: send message to server and wait for feedback to start
 
+    //missed: send message to server and wait for feedback to start
+    rc = write(my_socket,&atoi(argv[1]),sizeof(int));
     //loop of send random numbers to server:
     while(answer!=FINISH){
-    	rand_num=rand();
+    	rand_num=rand()%10;
     	rc = write(my_socket,&rand_num,sizeof(int));
     	nums_sent++;
     	if(rc<0)
     		terminate("cannot write to server\n");
     	rc=read(my_socket,&answer,sizeof(int));
-    	if(rc<0)
+    	if(rc<0){
+    		break;
     		terminate("cannot read from server\n");
-    	if(answer==YES)
+    	}
+    	if(answer==YES){
     		nums_deleted++;
+    	}
     }
-
     close(my_socket);
     freeaddrinfo(addr_info_res) ;
-    printf ("%d %d",nums_sent,nums_deleted);
+    printf ("%d %d\n",nums_sent,nums_deleted);
     return EXIT_SUCCESS;
 }
 //=========================================================================
